b7a158446fb8091361cd29472278d5d422ddd6af
Greg <greg@Gregs-MacBook-Pro.local>
2021-02-02 13:43:49 -0500

    files in src

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import NavBar from './components/navbar';
 import './App.css';
+import Counters from './components/counters';
+import AstroYantra from './components/astroYantra';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    counters: [
+        { id: 1, count: 4 },
+        { id: 2, count: 0 },
+        { id: 3, count: 0 },
+        { id: 4, count: 0 }
+    ]
+  };
+
+  constructor(props) {
+    super(props);
+    console.log('app - constructor');
+  }
+
+  componentDidMount() {
+    // can add Ajax Call to get data from server here
+    console.log('app - mounted');
+  }
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = {...counter};
+    counters[index].count++;
+    this.setState({ counters });
+  }
+
+  handleDelete = (counterId) => {
+    console.log('Event handler called', counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters })
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+        c.count = 0;
+        return c;
+    });
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar totalCounters = {this.state.counters.filter(c => c.count >0).length} />
+        <main className="container">
+          <Counters 
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+          <AstroYantra moleculeScale="10" />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
-
 export default App;
