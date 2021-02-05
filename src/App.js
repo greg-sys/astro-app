6649303902b69ddd75ec878f907ac951a7efeb51
Greg <greg@Gregs-MacBook-Pro.local>
2021-02-05 11:51:00 -0500

    changed html text 2

--- src/App.js
+++ src/App.js
@@ -56,11 +56,11 @@ class App extends Component {
           </div>
           <div class="column">
               <h1>Welcome to my Home Page!</h1>
-              Here is my astrological yantra in two forms. These diagrams show the positions of the planets in the sidereal zodiac signs. Aries is at the top, and the signs proceed through Taurus, Gemini, and so on going counterclockwise.
+              Here is my astrological yantra in two forms. These diagrams show the positions of the planets in the sidereal zodiac signs. Aries is at the top, and the signs proceed through Taurus, Gemini, and so on going counterclockwise.<br />
 
-              The design is based on this <a href ="https://commons.wikimedia.org/wiki/File:Thai_wheel_with_detriment.svg">astrological wheel</a> from Wikimedia Commons.
+              The design is based on this <a href ="https://commons.wikimedia.org/wiki/File:Thai_wheel_with_detriment.svg">astrological wheel</a> from Wikimedia Commons.<br />
 
-              The implementation is via the <a href="https://www.astro.com/swisseph/swephinfo_e.htm">Swiss Ephemeris</a>, using the standard Lahiri ayanamsa and the true lunar nodes.
+              The implementation is via the <a href="https://www.astro.com/swisseph/swephinfo_e.htm">Swiss Ephemeris</a>, using the standard Lahiri ayanamsa and the true lunar nodes.<br />
           </div>
           <div class="rightcolumn">
             <AstroYantra symbols="Western" />
