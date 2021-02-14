c38e0dd0f6a73c0442b17fd1e1470002e5d5b0d3
Greg <greg@Gregs-MacBook-Pro.local>
2021-02-14 15:36:07 -0500

    react-responsive added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import AstroYantra from './components/astroYantra';
+import MediaQuery from 'react-responsive';
 
 class App extends Component {
 
@@ -18,22 +19,42 @@ class App extends Component {
     return (
       <React.Fragment>
         <main className="container">
-          <div class="leftcolumn">
-            <AstroYantra symbols="Thai" />
-          </div>
-          <div class="column">
+        <MediaQuery maxWidth={1224}>
+            <div class="topblock">
+              <AstroYantra symbols="Thai" />
+            </div>
+            <div class="middleblock">
               <h1>Where are the planets?</h1>
               <div font-size="18px">
-                Here are two React components rendered with Thai numerals on the left and Western planetary symbols on the right. These diagrams show the current positions of the planets in the sidereal zodiac signs. Aries is at the top, and the signs proceed counterclockwise through Taurus, Gemini, and so on.<br /><br />
+                Here are two React components rendered with Thai numerals and Western planetary symbols. These diagrams show the current positions of the planets in the sidereal zodiac signs. Aries is at the top, and the signs proceed counterclockwise through Taurus, Gemini, and so on.<br /><br />
 
                 The design is based on this <a href ="https://commons.wikimedia.org/wiki/File:Thai_wheel_with_detriment.svg">astrological wheel</a> from Wikimedia Commons.<br /><br />
 
                 The implementation is via the <a href="https://www.astro.com/swisseph/swephinfo_e.htm">Swiss Ephemeris</a>, using the standard Lahiri ayanamsa and the true lunar nodes.<br /><br />
               </div>
-          </div>
-          <div class="rightcolumn">
-            <AstroYantra symbols="Western" />
-          </div>
+            </div>
+            <div class="bottomblock">
+              <AstroYantra symbols="Western" />
+            </div>
+          </MediaQuery>
+          <MediaQuery minWidth={1224}>
+            <div class="leftcolumn">
+              <AstroYantra symbols="Thai" />
+            </div>
+            <div class="column">
+                <h1>Where are the planets?</h1>
+                <div font-size="18px">
+                  Here are two React components rendered with Thai numerals and Western planetary symbols. These diagrams show the current positions of the planets in the sidereal zodiac signs. Aries is at the top, and the signs proceed counterclockwise through Taurus, Gemini, and so on.<br /><br />
+
+                  The design is based on this <a href ="https://commons.wikimedia.org/wiki/File:Thai_wheel_with_detriment.svg">astrological wheel</a> from Wikimedia Commons.<br /><br />
+
+                  The implementation is via the <a href="https://www.astro.com/swisseph/swephinfo_e.htm">Swiss Ephemeris</a>, using the standard Lahiri ayanamsa and the true lunar nodes.<br /><br />
+                </div>
+            </div>
+            <div class="rightcolumn">
+              <AstroYantra symbols="Western" />
+            </div>
+          </MediaQuery>
         </main>
       </React.Fragment>
     );
