a014d4c023c246a59276c02164db48d9683cd111
Greg <greg@Gregs-MacBook-Pro.local>
2021-02-18 15:10:36 -0500

    trying to start two node instances in package.json script

--- src/home.js
+++ src/home.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const { response } = require('express');
 
 router.get('/', (req, res, next) => {
-    res.render('index', { title: 'Express' });
+    // res.sendFile('./index', { title: 'Express' });
+    res.send("Hello world!", {title: 'Express' });
 });
 
 module.exports = router;
