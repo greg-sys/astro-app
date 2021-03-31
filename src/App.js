d9747828ad062db9d025ef016a83d8ffb99b2ed5
Greg <greg@Gregs-MacBook-Pro.local>
2021-03-31 12:18:05 -0400

    credits added 2

--- src/App.js
+++ src/App.js
@@ -51,6 +51,8 @@ class App extends Component {
                   The design is based on this <a href ="https://commons.wikimedia.org/wiki/File:Thai_wheel_with_detriment.svg">astrological wheel</a> from Wikimedia Commons.<br /><br />
 
                   The implementation is via the <a href="https://www.astro.com/swisseph/swephinfo_e.htm">Swiss Ephemeris</a>, using the standard Lahiri ayanamsa and the true lunar nodes.<br /><br />
+
+                  This is a project by Greg Lyon. If you would like to see the source code, please check out <a href="https://github.com/greg-sys">my GitHub page</a>.
                 </div>
             </div>
             <div class="rightcolumn">
